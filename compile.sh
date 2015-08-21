c57d4b0be9465ba7e0a1ce5a083fc65ba66b974b
Liz Abinante <eabinante@gmail.com>
2015-08-20 19:23:36 -0700

    beautiful amazing shell script for build/deploy/version :clap:

--- compile.sh
+++ compile.sh
@@ -3,10 +3,8 @@ compass compile -e production --force --css-dir=stylesheets/$1
 
 mkdir builds/$1
 mkdir builds/$1/css
-cp -r stylesheets/$1 builds/$1/css
-
-mkdir builds/$1/sass
-cp -r sass builds/$1/sass
+cp -r stylesheets/$1/ builds/$1/css
+cp -r sass builds/$1
 
 s3_website push
 
