fd02ff9ef76cae4b18e858c9b96236fa86e3191f
Liz Abinante <eabinante@gmail.com>
2015-08-20 19:21:50 -0700

    build & deploy version 0.0.0 [TEST, the sequel]

--- compile.sh
+++ compile.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 compass compile -e production --force --css-dir=stylesheets/$1
+
+mkdir builds/$1
+mkdir builds/$1/css
+cp -r stylesheets/$1 builds/$1/css
+
+mkdir builds/$1/sass
+cp -r sass builds/$1/sass
+
 s3_website push
+
+git add builds
+git add stylesheets
+git commit -m "build & deploy version $1 [$2]"
